99ec3d2ec7d9331b425d5df114508e6861a06c29
U5KUN <u5kunkousiki@gmail.com>
2025-05-21 08:14:21 +0900

    OS検出用にUAParserを導入

--- download/script.js
+++ download/script.js
@@ -1,23 +1,9 @@
 const os = document.getElementById("os");
 const selectOS = document.getElementById("selectOS");
-const usa = navigator.userAgent;
-const ios = parseFloat(('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(usa) || [0, ''])[1]).replace('undefined', '3_2').replace('_', '.').replace('_', ''));
-const detectedOS = detectOS();
-function detectOS() {
-    var ua = usa.toLowerCase();
-    let OS;
-    if (ua.indexOf("iphone") !== -1 || ua.indexOf("ipad") !== -1) {
-        OS = "iOS/iPadOS";
-    } else if (ua.indexOf("mac os x") !== -1) {
-        OS = "macOS";
-    } else if (ua.indexOf("windows nt") !== -1) {
-        OS = "Windows";
-    } else if (ua.indexOf("android") !== -1) {
-        OS = "Android";
-    } else {
-        OS = "不明";
-    }
-    return OS;
+const uap = new UAParser();
+let detectedOS = uap.getOS();
+if (detectedOS.name == "iOS") {
+    detectedOS.name = "iOS/iPadOS";
 }
 function showContent(OS) {
     document.querySelectorAll("main section.hidden").forEach((section) => {
@@ -26,9 +12,9 @@ function showContent(OS) {
     document.getElementById(OS).style.display = "block";
     selectOS.value = OS;
 }
-if (ios && ios <= 17.0) { document.getElementById("ts").style.display = "block"; }
-os.innerText = detectedOS;
-showContent(detectedOS);
+if (detectedOS.name == "iOS" && detectedOS.version <= 17.0) { document.getElementById("ts").style.display = "block"; }
+os.innerText = detectedOS.name;
+showContent(detectedOS.name);
 
 // セレクトボックスの変更イベントを監視
 selectOS.addEventListener('change', (e) => {
